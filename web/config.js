c5ff454de93ec785b4b2e0f30277d36d429ad7b4
saulpanzer23 <saul.panzer23@gmail.com>
2015-09-09 21:10:54 -0700

    Update config.js

--- web/config.js
+++ web/config.js
@@ -1,7 +1,6 @@
 benchmarkConfig({
   "tests": "http://vdom-benchmark.github.io/vdom-benchmark/tests.js",
   "contestants": [
-    
     {
       "name": "morphdom2",
       "url": "https://github.com/patrick-steele-idem/morphdom",
