a7e1b41f6df6468dc0c3426d3917339eb172245b
saulpanzer23 <saul.panzer23@gmail.com>
2015-09-09 21:09:27 -0700

    Update config.js

--- web/config.js
+++ web/config.js
@@ -1,85 +1,11 @@
 benchmarkConfig({
   "tests": "http://vdom-benchmark.github.io/vdom-benchmark/tests.js",
   "contestants": [
+    
     {
-      "name": "uix",
-      "url": "https://github.com/localvoid/uix",
-      "benchmarkUrl": "https://localvoid.github.io/vdom-benchmark-uix/"
-    },
-    {
-      "name": "cito.js",
-      "url": "https://github.com/joelrich/citojs",
-      "benchmarkUrl": "https://vdom-benchmark.github.io/vdom-benchmark-cito/"
-    },
-    {
-      "name": "Bobril",
-      "url": "https://github.com/Bobris/Bobril",
-      "benchmarkUrl": "https://vdom-benchmark.github.io/vdom-benchmark-bobril/"
-    },
-    {
-      "name": "React",
-      "url": "http://facebook.github.io/react/",
-      "benchmarkUrl": "https://vdom-benchmark.github.io/vdom-benchmark-react/"
-    },
-    {
-      "name": "virtual-dom",
-      "url": "https://github.com/Matt-Esch/virtual-dom",
-      "benchmarkUrl": "https://vdom-benchmark.github.io/vdom-benchmark-virtual-dom/"
-    },
-    {
-      "name": "mithril",
-      "url": "http://lhorie.github.io/mithril/",
-      "benchmarkUrl": "https://vdom-benchmark.github.io/vdom-benchmark-mithril/"
-    },
-    {
-      "name": "maquette",
-      "url": "http://github.com/AFASSoftware/maquette/",
-      "benchmarkUrl": "https://vdom-benchmark.github.io/vdom-benchmark-maquette/"
-    },
-    {
-      "name": "dom-layer",
-      "url": "https://github.com/crysalead-js/dom-layer/",
-      "benchmarkUrl": "https://vdom-benchmark.github.io/vdom-benchmark-dom-layer/"
-    },
-    {
-      "name": "Snabbdom",
-      "url": "https://github.com/paldepind/snabbdom/",
-      "benchmarkUrl": "https://paldepind.github.io/vdom-benchmark-snabbdom/"
-    },
-    {
-      "name": "Deku",
-      "url": "https://github.com/dekujs/deku/",
-      "benchmarkUrl": "https://dekujs.github.io/vdom-benchmark/"
-    },
-    {
-      "name": "Trackira",
-      "url": "https://github.com/trackira/trackira",
-      "benchmarkUrl": "https://deamme.github.io/vdom-benchmark-trackira/"
-    },
-    {
-      "name": "Incremental DOM",
-      "url": "https://github.com/google/incremental-dom",
-      "benchmarkUrl": "https://vdom-benchmark.github.io/vdom-benchmark-idom/"
-    },
-    {
-      "name": "InfernoJS",
-      "url": "https://github.com/trueadm/inferno",
-      "benchmarkUrl": "http://vdom-benchmark.github.io/vdom-benchmark-inferno/"
-    },
-    {
-      "name": "morphdom",
+      "name": "morphdom2",
       "url": "https://github.com/patrick-steele-idem/morphdom",
       "benchmarkUrl": "http://vdom-benchmark.github.io/vdom-benchmark-morphdom/"
-    },
-    {
-      "name": "Vidom",
-      "url": "https://github.com/dfilatov/vidom/",
-      "benchmarkUrl": "https://dfilatov.github.io/vdom-benchmark-vidom/"
-    },
-    {
-      "name": "kivi",
-      "url": "https://github.com/localvoid.github.io/kivi",
-      "benchmarkUrl": "https://vdom-benchmark.github.io/vdom-benchmark-kivi/"
     }
   ]
 });
